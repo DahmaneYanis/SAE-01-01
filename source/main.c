4f48f24358e886fd07849988651f31fdafa0cfb1
DahmaneYanis <dahmane.yanis.2004@gmail.com>
2022-11-12 01:58:49 +0100

    Test

--- source/main.c
+++ source/main.c
@@ -7,6 +7,7 @@ int main(void){
     #endif
 
     MenuGlobal();
+    // 
 
     /*int tabCarte[17] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
     int tabEtat[17] = {1, 1, 1, 1, 1, 1, -2, 1, 1, 1, 1, 1, 1, 1, 1};
